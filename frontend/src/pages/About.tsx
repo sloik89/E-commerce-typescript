44ef43f488e54be02c25f1e449cb323927ce9c57
sloik89 <sloik89@gmail.com>
2024-04-27 14:27:13 +0200

    adding about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="">
+      <h2 className="flex flex-wrap text-5xl text-center font-bold tracking-wide leading-none gap-2 sm:text-6xl items-center justify-center sm:gap-x-6">
+        We love
+        <span className="bg-primary text-white rounded-lg">comfy</span>
+      </h2>
+      <p className="text-lg mt-6 mx-auto leading-8 max-w-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa repellat
+        illo provident animi earum! Dicta hic accusamus cupiditate labore nemo
+        praesentium, dolor cum ullam quod rem a asperiores rerum sapiente
+        reiciendis voluptas numquam ex mollitia veritatis expedita vitae laborum
+        repellat perspiciatis nam! Maxime nesciunt reiciendis unde
+        exercitationem, nisi repudiandae debitis!
+      </p>
+    </section>
+  );
 };
 
 export default About;
