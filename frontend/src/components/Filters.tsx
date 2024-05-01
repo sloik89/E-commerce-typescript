7874a611769d191e17ae316619dcf9756b19bbf2
sloik89 <sloik89@gmail.com>
2024-05-01 14:44:08 +0200

    working on fillters

--- frontend/src/components/Filters.tsx
+++ frontend/src/components/Filters.tsx
@@ -4,9 +4,23 @@ import { Button } from "./ui/button";
 import { Form, useLoaderData, Link } from "react-router-dom";
 const Filters = () => {
   return (
-    <Form className="border-2 border-secondary rounded-md px-8 py-4">
-      Filters
-      <div></div>
+    <Form className="border border-secondary rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
+      <div className="mb-2">
+        <Label htmlFor="search">Search Product</Label>
+        <Input id="search" name="search" defaultValue="" />
+      </div>
+      <Button type="submit" size="sm" className="self-end mb-2">
+        Search
+      </Button>
+      <Button
+        type="button"
+        asChild
+        size="sm"
+        variant="outline"
+        className="self-end mb-2"
+      >
+        <Link to="/products">reset</Link>
+      </Button>
     </Form>
   );
 };
