b0e22d51deaa61da88e92e92b1698ee395066213
sloik89 <sloik89@gmail.com>
2024-06-08 13:53:30 +0200

    adding auth frontend

--- frontend/src/features/user/userSlice.ts
+++ frontend/src/features/user/userSlice.ts
@@ -1,10 +1,33 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-const initialState = {
-  name: "user slice",
+import { toast } from "@/components/ui/use-toast";
+export type User = {
+  username: string;
+};
+type UserState = {
+  user: User | null;
+};
+const getUserFromLocalStorage = (): User | null => {
+  const user = localStorage.getItem("user");
+  if (!user) return null;
+  return JSON.parse(user);
+};
+const initialState: UserState = {
+  user: getUserFromLocalStorage(),
 };
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {},
+  reducers: {
+    loginUser: (state, action: PayloadAction<User>) => {
+      const user = action.payload;
+      localStorage.setItem("user", JSON.stringify("user"));
+      toast({ description: "Login successful" });
+    },
+    logoutUser: (state) => {
+      state.user = null;
+      localStorage.removeItem("user");
+    },
+  },
 });
 export default userSlice.reducer;
+export const { loginUser, logoutUser } = userSlice.actions;
